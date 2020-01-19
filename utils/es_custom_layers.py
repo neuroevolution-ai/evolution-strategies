a95ac8de7bab01abd81d8d5ed59b9068458eeb11
Patrick Deubel <patrick.deubel@gmail.com>
2020-01-19 15:01:06 +0100

    Added the custom layers to its own file.

--- utils/es_custom_layers.py
+++ utils/es_custom_layers.py
@@ -1,1 +1,69 @@
-# TODO import custom layers here and check if this works with importing tensorflow inside a function
+import numpy as np
+import tensorflow as tf
+
+
+class Normc_initializer(tf.keras.initializers.Initializer):
+    def __init__(self, std=1.0):
+        self.std = std
+
+    def __call__(self, shape, dtype=None, partition_info=None):
+        out = np.random.randn(*shape).astype(np.float32)
+        out *= self.std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
+        return tf.constant(out)
+
+
+class ObservationNormalizationLayer(tf.keras.layers.Layer):
+    def __init__(self, ob_mean, ob_std, **kwargs):
+        self.ob_mean = ob_mean
+        self.ob_std = ob_std
+        super(ObservationNormalizationLayer, self).__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        return tf.clip_by_value((inputs - self.ob_mean) / self.ob_std, -5.0, 5.0)
+
+    # get_config and from_config need to implemented to be able to serialize the model
+    def get_config(self):
+        base_config = super(ObservationNormalizationLayer, self).get_config()
+        base_config['ob_mean'] = self.ob_mean
+        base_config['ob_std'] = self.ob_std
+        return base_config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
+class DiscretizeActionsUniformLayer(tf.keras.layers.Layer):
+    def __init__(self, num_ac_bins, adim, ahigh, alow, **kwargs):
+        self.num_ac_bins = num_ac_bins
+        self.adim = adim
+        # ahigh, alow are NumPy arrays when extracting from the environment, but when the model is loaded from a h5
+        # File they get initialised as a normal list, where operations like subtraction does not work, thereforce
+        # cast them explicitly
+        self.ahigh = np.array(ahigh)
+        self.alow = np.array(alow)
+        super(DiscretizeActionsUniformLayer, self).__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        # Reshape to [n x i x j] where n is dynamically chosen, i equals action dimension and j equals the number
+        # of bins
+        scores_nab = tf.reshape(inputs, [-1, self.adim, self.num_ac_bins])
+        # This picks the bin with the greatest value
+        a = tf.argmax(scores_nab, 2)
+
+        # Then transform the interval from [0, num_ac_bins - 1] to [-1, 1] which equals alow and ahigh
+        ac_range_1a = (self.ahigh - self.alow)[None, :]
+        return 1. / (self.num_ac_bins - 1.) * tf.keras.backend.cast(a, 'float32') * ac_range_1a + self.alow[None, :]
+
+    def get_config(self):
+        # get_config and from_config need to implemented to be able to serialize the model
+        base_config = super(DiscretizeActionsUniformLayer, self).get_config()
+        base_config['num_ac_bins'] = self.num_ac_bins
+        base_config['adim'] = self.adim
+        base_config['ahigh'] = self.ahigh
+        base_config['alow'] = self.alow
+        return base_config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
