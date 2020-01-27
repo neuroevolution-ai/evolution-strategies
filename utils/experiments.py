70322ba7415b440feb499820d4c8b04abe40fbb0
Patrick Deubel <patrick.deubel@gmail.com>
2020-01-27 19:36:11 +0100

    Added the get_training_state method to TrainingRun.

--- utils/experiments.py
+++ utils/experiments.py
@@ -265,197 +265,32 @@ class TrainingRun:
                     continue
 
     def get_training_state(self, generation=-1):
-
+        """Returns the training state from the given generation. If the model and other files do not exist for this
+        generation, only the configuration is returned.
+        :param generation: Indicates the point from which the training state shall be returned, defaults to -1 (latest)
+        :return: Optimizations, ModelStructure, Config objects first, then model files, observation normalization files,
+            and optimizer files if they exist for the given generation
         """
-        TODO
-        1. assert generation >= 0 oder -1
-        2. Entsprechende files returnen. Wenn es model file nicht gibt -> error. Wenn es ob_norm oder optimizer nicht gibt,
-        jeweils None returnen muss enstprechend beim Aufrufer gehandled werden
-        """
-        pass
+        if generation == -1:
+            # We are using a dict so we need to do a little more to get the last generation
+            generation = list(self.model_files.keys())[-1]
 
+        try:
+            model_file = self.model_files[generation]
+        except KeyError or NameError:
+            return self.optimizations, self.model_structure, self.config, None, None, None
 
-        #
-        # if not isinstance(video_files, list):
-        #     self.video_files = []
-        # else:
-        #     self.video_files = video_files
-        #
-        # if not isinstance(model_files, list):
-        #     self.model_files = []
-        # else:
-        #     self.model_files = model_files
-        #
-        # if not isinstance(ob_normalization_files, list):
-        #     self.ob_normalization_files = []
-        # else:
-        #     self.ob_normalization_files = ob_normalization_files
-        #
-        # if not isinstance(optimizer_files, list):
-        #     self.optimizer_files = []
-        # else:
-        #     self.optimizer_files = optimizer_files
+        try:
+            ob_normalization_file = self.ob_normalization_files[generation]
+        except KeyError or NameError:
+            ob_normalization_file = None
 
-    # def get_training_state(self):
-    #
-    #     current_model_file, current_optimizer_file, current_ob_normalization_file = None, None, None
-    #
-    #     if self.model_files is not None:
-    #         current_model_file = self.model_files[-1]
-    #
-    #     if self.current
-    #
-    #     return self.optimizations, self.model_structure, self.config
-    #     pass
+        try:
+            optimizer_file = self.optimizer_files[generation]
+        except KeyError or NameError:
+            optimizer_file = None
 
-    # def __init__(self, save_directory, log, config, model_file_paths, evaluation=None, video_file=None):
-    #     self.save_directory = save_directory
-    #     self.log = log
-    #     self.config = config
-    #
-    #     if not model_file_paths:
-    #         self.no_models = True
-    #         self.model_file_paths = None
-    #     else:
-    #         self.no_models = False
-    #         self.model_file_paths = [os.path.join(save_directory, model) for model in model_file_paths]
-    #
-    #     if evaluation is not None:
-    #         self.no_evaluation = False
-    #         self.evaluation = evaluation
-    #         self.data = self.merge_log_eval()
-    #     else:
-    #         self.no_evaluation = True
-    #         self.evaluation = None
-    #         self.data = None
-    #
-    #     if video_file is not None:
-    #         self.no_video = False
-    #     else:
-    #         self.no_video = True
-    #     self.video_file = video_file
-    #
-    #     if self.log is None or self.config is None:
-    #         print("This TrainingRun is missing either the log file or the configuration file. It will not "
-    #               + "work as expected.")
-    #
-    # def merge_log_eval(self):
-    #     if self.log is not None and self.evaluation is not None:
-    #         return self.log.merge(self.evaluation[['Generation', 'Eval_Rew_Mean', 'Eval_Rew_Std', 'Eval_Len_Mean']],
-    #                               on='Generation')
-    #     return None
-    #
-    # def parse_generation_number(self, model_file_path):
-    #     try:
-    #         number = int(model_file_path.split('snapshot_')[-1].split('.h5')[0])
-    #         return number
-    #     except ValueError:
-    #         return None
-    #
-    # def evaluate(self, force=False, eval_count=5, skip=None, save=False, delete_models=False):
-    #     if not force:
-    #         if self.data is not None:
-    #             return self.data
-    #
-    #     if self.no_models:
-    #         print("No models given for that training run, so no new evaluation is possible. You can still plot" +
-    #               " your data if you have an evaluation.csv or log.csv.")
-    #         return None
-    #
-    #     head_row = ['Generation', 'Eval_per_Gen', 'Eval_Rew_Mean', 'Eval_Rew_Std', 'Eval_Len_Mean']
-    #
-    #     for i in range(eval_count):
-    #         head_row.append('Rew_' + str(i))
-    #         head_row.append('Len_' + str(i))
-    #
-    #     data = []
-    #
-    #     results_list = []
-    #     pool = Pool(os.cpu_count())
-    #
-    #     for model_file_path in self.model_file_paths[::skip]:
-    #         results = []
-    #         gen = self.parse_generation_number(model_file_path)
-    #
-    #         for _ in range(eval_count):
-    #             results.append(pool.apply_async(func=self.run_model, args=(model_file_path,)))
-    #         results_list.append((results, gen))
-    #
-    #     for (results, gen) in results_list:
-    #         for i in range(len(results)):
-    #             results[i] = results[i].get()
-    #             if results[i] == [None, None]:
-    #                 print("The provided model file produces non finite numbers. Stopping.")
-    #                 return
-    #
-    #         rewards = np.array(results)[:, 0]
-    #         lengths = np.array(results)[:, 1]
-    #
-    #         row = [gen,
-    #                eval_count,
-    #                np.mean(rewards),
-    #                np.std(rewards),
-    #                np.mean(lengths)]
-    #
-    #         assert len(rewards) == len(lengths)
-    #         for i in range(len(rewards)):
-    #             row.append(rewards[i])
-    #             row.append(lengths[i])
-    #
-    #         data.append(row)
-    #
-    #     pool.close()
-    #     pool.join()
-    #
-    #     self.evaluation = pd.DataFrame(data, columns=head_row)
-    #     if save:
-    #         self.save_evaluation()
-    #     # Only copy the mean values in the merged data
-    #     self.data = self.merge_log_eval()
-    #
-    #     if delete_models:
-    #         self.delete_model_files
-    #
-    #     return self.data
-    #
-    # def delete_model_files(self, save_last=False):
-    #     if save_last:
-    #         self.model_file_paths = self.model_file_paths[:-1]
-    #     for model_file_path in self.model_file_paths:
-    #         os.remove(model_file_path)
-    #
-    # def plot_reward_timestep(self):
-    #     if self.data is not None:
-    #         plot(self.data.TimestepsSoFar, 'Timesteps', self.data.Eval_Rew_Mean, 'Cummulative reward')
-    #     else:
-    #         print("You did not evaluate these results. The evaluated mean reward displayed was computed during training"
-    #               + "and can have missing values!")
-    #         plot(self.log.TimestepsSoFar, 'Timesteps', self.log.EvalGenRewardMean, 'Cummulative reward')
-    #
-    # def save_evaluation(self):
-    #     if self.evaluation is not None:
-    #         self.evaluation.to_csv(os.path.join(self.save_directory, 'evaluation.csv'))
-    #
-    # def visualize(self, force=False):
-    #     if self.no_models:
-    #         # Error message in Experiment
-    #         return None
-    #     if not force:
-    #         if self.video_file is not None:
-    #             return self.video_file
-    #
-    #     latest_model = self.model_file_paths[-1]
-    #
-    #     with Pool(os.cpu_count()) as pool:
-    #         pool.apply(func=self.run_model, args=(latest_model, True))
-    #
-    #     for file in os.listdir(self.save_directory):
-    #         if file.endswith('.mp4'):
-    #             self.video_file = os.path.join(self.save_directory, file)
-    #
-    #     return self.video_file
-    #
-
+        return self.optimizations, self.model_structure, self.config, model_file, ob_normalization_file, optimizer_file
 
 
 class Experiment:
